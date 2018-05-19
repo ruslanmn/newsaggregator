b5ce14214c89c8a3190ad7b226f10b7dc841c452
kmeansfan <kmeansfan@gmail.com>
2018-05-19 21:36:32 +0400

    NA news documents

--- src/main/java/storage/sourceparsers/SourceParserFactory.java
+++ src/main/java/storage/sourceparsers/SourceParserFactory.java
@@ -1,19 +1,19 @@
 package storage.sourceparsers;
 
 import storage.Source;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import java.lang.UnsupportedOperationException;
 
 public class SourceParserFactory {
 
     private final static LentaParser lentaParser = new LentaParser();
     private final static RamblerParser ramblerParser = new RamblerParser();
 
-    public static SourceParser get(Source source) throws NotImplementedException {
+    public static SourceParser get(Source source) throws UnsupportedOperationException {
         if(source.getUrl().contains("lenta.ru"))
             return lentaParser;
         else if(source.getUrl().contains("rambler.ru"))
             return ramblerParser;
         else
-            throw new NotImplementedException();
+            throw new UnsupportedOperationException();
     }
 }
